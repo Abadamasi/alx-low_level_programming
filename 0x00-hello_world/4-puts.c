f67f557351cba125453eb20235bc782e50a150c6
Abadamasi <abbabadamasi33@gmail.com>
2023-05-19 04:14:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
+ *
  * main - C program that prints single line
  *
  */
-void main - c is fun
+int main(void)
+
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
