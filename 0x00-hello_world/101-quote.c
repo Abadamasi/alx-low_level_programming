d3ab8e26f6ebd7f0502b839c9186386b3a9bc0a0
Abadamasi <abbabadamasi33@gmail.com>
2023-05-22 03:24:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,10 @@
  *
  * Description: prints out the last part of a quote in the standard error.
  *
- * Return: Always 1 if Success
+ * Return: Always 1 if Success.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
