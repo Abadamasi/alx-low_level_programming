afe5b8844fedca57adb4753db975cb5890cd4462
Abadamasi <abbabadamasi33@gmail.com>
2023-06-06 16:55:43 -0700

    my primary goal of hacking

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -3,7 +3,7 @@
  *
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: on success 0
+ * Return: on success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 
