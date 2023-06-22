9cc46bc48fe958873ee1b5a039b5252ad8231ae0
Abadamasi <abbabadamasi33@gmail.com>
2023-06-22 12:24:50 -0700

    task 1

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+
+/**
+ * op_add - runs a addition calculation on a and b;
+ * @a: int value.
+ * @b: int value.
+ *
+ * Return: Resault of math operation.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - runs a subtraction calculation on a and b;
+ * @a: int value.
+ * @b: int value.
+ *
+ * Return: Resault of math operation.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
