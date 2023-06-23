b738ab604dfd9325ef463b264814e2a9b61c9179
Abadamasi <abbabadamasi33@gmail.com>
2023-06-22 22:26:24 -0700

    my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#include <stddef.h>
 #ifndef FUCTIONPOINTERS_FUNCTION_POINTERS_H
 #define FUCTIONPOINTERS_FUNCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
