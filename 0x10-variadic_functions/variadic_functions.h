a479486a63530921c06d1c48f6cc0fe9eb5aa881
Abadamasi <abbabadamasi33@gmail.com>
2023-06-23 17:34:57 -0700

    beauty is variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
-#ifndef "variadic_functions.h"
-#define "variadic_functions.h"
+#ifndef VARIADICFUNCTIONSH
+#define VARIADICFUNCTIONSH
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
